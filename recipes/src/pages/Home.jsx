8cd282689bdd0f4fc3fd049240f768034b3c5c9d
graefft <rustygraeff@gmail.com>
2022-12-24 18:47:39 -0800

    first commit

---
+++ recipes/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Vegetarian from "../components/Vegetarian";
+import Popular from "../components/Popular";;
+
+function Home() {
+  return (
+    <div>
+        <Vegetarian />
+        <Popular />
+    </div>
+  )
+}
+
+export default Home;
---
+++ recipes/src/pages/Pages.jsx
@@ -0,0 +1,21 @@
+import Home from './Home'
+import { Route, Routes } from 'react-router-dom';
+import Cuisine from './Cuisine';
+
+
+
+
+function Pages() {
+  return (
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cuisine/:type' element={<Cuisine />} />
+        {/* <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home />} /> */}
+
+      </Routes>
+  )
+}
+
+export default Pages;
